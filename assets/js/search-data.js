e86f003f40f30857a96a3c53379c13b863280b12
mbalazs98 <49280623+mbalazs98@users.noreply.github.com>
2025-10-27 12:59:41 +0000

    Deploying to gh-pages from @ mbalazs98/mbalazs98.github.io@8dcbc1f4b8da37b99808084a6e6b8aeb9eb4532a 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -37,21 +37,7 @@ ninja.data = [{
           handler: () => {
             window.location.href = "/cv/";
           },
-        },{id: "dropdown-bookshelf",
-              title: "bookshelf",
-              description: "",
-              section: "Dropdown",
-              handler: () => {
-                window.location.href = "/books/";
-              },
-            },{id: "dropdown-blog",
-              title: "blog",
-              description: "",
-              section: "Dropdown",
-              handler: () => {
-                window.location.href = "/blog/";
-              },
-            },{id: "post-efficient-event-based-delay-learning",
+        },{id: "post-efficient-event-based-delay-learning",
         
           title: "Efficient event-based delay learning",
         
